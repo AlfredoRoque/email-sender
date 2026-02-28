e9c54bb88eec5d09de272ea26374f698e9f4c784
AlfredoRoque <alfredorokeromero@hotmail.com>
2026-02-27 21:08:36 -0600

    se actualiza ruta de certificados con archivos

--- src/main/java/com/roridea/email_sender/consumer/KafkaCertLoader.java
+++ src/main/java/com/roridea/email_sender/consumer/KafkaCertLoader.java
@@ -1,10 +1,8 @@
 package com.roridea.email_sender.consumer;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Base64;
@@ -12,20 +10,14 @@ import java.util.Base64;
 @Component
 public class KafkaCertLoader {
 
-    @Value("${KAFKA_TRUSTSTORE_BASE64}")
-    private String truststoreBase64;
-
-    @Value("${KAFKA_KEYSTORE_BASE64}")
-    private String keystoreBase64;
-
     @PostConstruct
-    public void loadCerts() throws Exception {
-        createFileFromBase64(keystoreBase64, "/tmp/client.keystore.jks");
-        createFileFromBase64(truststoreBase64, "/tmp/client.truststore.jks");
-    }
+    public void init() throws Exception {
+        // Keystore
+        String keystoreBase64 = System.getenv("KAFKA_KEYSTORE_BASE64");
+        Files.write(Paths.get("/tmp/client.keystore.jks"), Base64.getDecoder().decode(keystoreBase64));
 
-    private void createFileFromBase64(String base64, String path) throws IOException {
-        byte[] decoded = Base64.getDecoder().decode(base64);
-        Files.write(Paths.get(path), decoded);
+        // Truststore
+        String truststoreBase64 = System.getenv("KAFKA_TRUSTSTORE_BASE64");
+        Files.write(Paths.get("/tmp/client.truststore.jks"), Base64.getDecoder().decode(truststoreBase64));
     }
 }
